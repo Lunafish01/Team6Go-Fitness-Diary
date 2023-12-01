67236a9b0070e19b1d0a2bff3c7d4e7f23c1dfc5
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-11-30 20:28:30 -0600

    built steps model

--- models/steps.js
+++ models/steps.js
@@ -1,21 +1,55 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Food extends Model {}
+class Steps extends Model {}
 
-Food.init({
-  id: {
-
+Steps.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    step_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    calories_burned: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    distance_travelled: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  user_id: {
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "steps",
+  }
+);
 
-  },
-  food_name: {
-
-  },
-  calorie_count: {
-    
-  },
-});
-
-module.exports = Food;
+module.exports = Steps;
