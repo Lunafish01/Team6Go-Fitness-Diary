2054f0768c51420a9a6a9600e0eb1ed517fb069b
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-30 20:15:53 -0600

    updated models folder

--- models/food.js
+++ models/food.js
@@ -4,21 +4,43 @@ const sequelize = require("../config/connection");
 class Food extends Model {}
 
 Food.init(
-    {
-        id: {
-            
-        },
-        user_id:{
-
-        },
-        food_name: {
-
-        },
-        calorie_count: {
-
-        },
-    }
-    
-);
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    food_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    calorie_count: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "food",
+  }
+),
 
  module.exports = Food;
--- models/water.js
+++ models/water.js
@@ -4,18 +4,10 @@ const sequelize = require("../config/connection");
 class Food extends Model {}
 
 Food.init({
-  id: {
-
-  },
-  user_id: {
-
-  },
-  food_name: { 
-
-  },
-  calorie_count: {
-
-  },
+  id: {},
+  user_id: {},
+  food_name: {},
+  calorie_count: {},
 });
 
 module.exports = Food;
