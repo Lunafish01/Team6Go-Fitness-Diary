860af0f14852d3a5483ae55bb8d5dcde8f31d8c8
MaribelC <Maribel_Calvario@yahoo.com>
2023-12-01 22:42:55 -0600

    added seeds water data

---
+++ seeds/water.js
@@ -0,0 +1,26 @@
+const { water } = require('../models');
+
+const waterData = [
+    {
+        user_id: 1,
+        date: '2023-08-06',
+        daily_goal: 64,
+        actual_intake: 16,
+    },
+    {
+        user_id: 2,
+        date: '2023-09-28',
+        daily_goal: 128,
+        actual_intake: 64,
+    },
+    {
+        user_id: 3,
+        date: '2023-12-28',
+        daily_goal: 64,
+        actual_intake: 0,
+    }
+]
+
+const seedWater = () => water.bulkCreate(waterData);
+
+module.exports = seedWater;
