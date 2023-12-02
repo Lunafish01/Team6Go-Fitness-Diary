5a612c03fe45263282147c3f37060c0230ad7457
MaribelC <Maribel_Calvario@yahoo.com>
2023-12-02 13:27:05 -0600

    updated index.js routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const food = require('./food');
+const steps = require('./steps');
+const water = require('./water');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
