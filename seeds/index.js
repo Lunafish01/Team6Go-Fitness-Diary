12d814c8ecd49a7df4755c21c261707ad1e1b95b
MaribelC <Maribel_Calvario@yahoo.com>
2023-12-03 00:09:35 -0600

    added index.js to seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,21 @@
+const sequelize = require("../config/connection");
+const seedFood = require("./food-seeds");
+const seedSteps = require("./steps-seeds");
+const seedWater = require("./water-seeds");
+const seedUser = require("./user-seeds");
+
+const seedAll = async () => {
+    await sequelize.sync({ forces: true });
+
+    await seedUser();
+
+    await seedFood();
+
+    await seedSteps(); 
+
+    await seedWater(); 
+
+    process.exit(0);
+};
+
+seedAll();
