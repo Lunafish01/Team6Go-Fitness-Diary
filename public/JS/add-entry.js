ac3a91f396180a35652cbf04dc361dfcec243c74
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-12-03 23:15:55 -0600

    added JS to add-entry.js

---
+++ public/JS/add-entry.js
@@ -0,0 +1,27 @@
+async function newEntryHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const calories = document.querySelector('input[name="calories"]').value;
+  const servingSize = document.querySelector('input[name="serving-size"]').value;
+
+  const response = await fetch(`/api/foodRoutes`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      calories,
+      servingSize
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector(".new-food-entry").addEventListener("submit", newEntryHandler);
