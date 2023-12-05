f6c295acd9b25945783b1298693969c19bf9f8c6
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-12-04 21:56:59 -0600

    pushing to heroku

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: false,
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
