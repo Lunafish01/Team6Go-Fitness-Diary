c514d156640e01700430ee9fd16098f8d41f117e
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-12-04 23:03:56 -0600

    reworked edit food js and handlebar and created some blank files

--- public/JS/edit-entry.js
+++ public/JS/edit-entry.js
@@ -1,67 +1,46 @@
 // function to edit post
-async function editFormHandler(event) {
-  event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
-  const content = document
-    .querySelector('textarea[name="content"]')
-    .value.trim();
-
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  if (title && content) {
-    const foodResponse = await fetch(`/api/foodRoutes/${id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        post_id: id,
-        title,
-        content,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // If successful, redirect the browser to the dashboard page
-    if (foodResponse.ok) {
-      document.location.replace("/dashboard/");
-    } else {
-      const foodErrorMessage = await foodResponse.text();
-      alert(`Error editing post: ${foodErrorMessage}`);
-    }
-  }
-
-  // Additional API routes
-  const stepsResponse = await fetch(`/api/stepsRoutes/${id}`, {
-    method: "Put",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (stepsResponse.ok) {
-    // Handle the response for the second route
-  } else {
-    const stepsErrorMessage = await stepsResponse.text();
-    alert(`Error in steps route: ${stepsErrorMessage}`);
-  }
-
-  const waterResponse = await fetch(`/api/waterRoutes/${id}`, {
-    method: "Put",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (waterResponse.ok) {
-    // Handle the response for the third route
-  } else {
-    const waterErrorMessage = await waterResponse.text();
-    alert(`Error in water route: ${waterErrorMessage}`);
-  }
+// Extract entry ID from the URL
+function getEntryIdFromUrl() {
+  const id = window.location.pathname.split("/").pop();
+  return id;
 }
 
-document
-  .querySelector(".edit-post-form")
-  .addEventListener("submit", editFormHandler);
+document.addEventListener("DOMContentLoaded", function () {
+  const editFoodForm = document.querySelector(".edit-food-form");
+
+  editFoodForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const id = getEntryIdFromUrl(); 
+    
+    // Gather updated data from the form
+    const updatedData = {
+      name: document.getElementById("meal-name").value.trim(),
+      calories: document.getElementById("calories").value.trim(),
+      servingSize: document.getElementById("serving-size").value.trim(),
+    };
+
+    try {
+      // Make a PUT request to the food API route for editing
+      const response = await fetch(`/api/foodRoutes/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        alert("Food entry updated successfully!");
+        // Redirect or perform additional actions as needed
+      } else {
+        const errorMessage = await response.text();
+        alert(`Error updating food entry: ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error("Error updating food entry:", error);
+      alert("An error occurred while updating the food entry.");
+    }
+  });
+});
