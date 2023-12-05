a1316b1ba0c312c9920f5de8bf908a6c5aaf4a6b
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-12-05 00:03:26 -0600

    added edit entries for water and steps, plus handlebars

--- public/JS/edit-entry.js
+++ public/JS/edit-entry.js
@@ -1,46 +1,37 @@
 // function to edit post
+async function editEntryHandler(event) {
+  event.preventDefault();
 
-// Extract entry ID from the URL
-function getEntryIdFromUrl() {
-  const id = window.location.pathname.split("/").pop();
-  return id;
+  const id = getEntryIdFromUrl(); 
+  
+  // Gather updated data from the form
+  const updatedData = {
+    name: document.getElementById("meal-name").value.trim(),
+    calories: document.getElementById("calories").value.trim(),
+    servingSize: document.getElementById("serving-size").value.trim(),
+  };
+
+  try {
+    // Make a PUT request to the food API route for editing
+    const response = await fetch(`/api/foodRoutes/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(updatedData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      alert("Food entry updated successfully!");
+     
+    } else {
+      const errorMessage = await response.text();
+      alert(`Error updating food entry: ${errorMessage}`);
+    }
+  } catch (error) {
+    console.error("Error updating food entry:", error);
+    alert("An error occurred while updating the food entry.");
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const editFoodForm = document.querySelector(".edit-food-form");
-
-  editFoodForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
-
-    const id = getEntryIdFromUrl(); 
-    
-    // Gather updated data from the form
-    const updatedData = {
-      name: document.getElementById("meal-name").value.trim(),
-      calories: document.getElementById("calories").value.trim(),
-      servingSize: document.getElementById("serving-size").value.trim(),
-    };
-
-    try {
-      // Make a PUT request to the food API route for editing
-      const response = await fetch(`/api/foodRoutes/${id}`, {
-        method: "PUT",
-        body: JSON.stringify(updatedData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        alert("Food entry updated successfully!");
-        // Redirect or perform additional actions as needed
-      } else {
-        const errorMessage = await response.text();
-        alert(`Error updating food entry: ${errorMessage}`);
-      }
-    } catch (error) {
-      console.error("Error updating food entry:", error);
-      alert("An error occurred while updating the food entry.");
-    }
-  });
-});
+document.querySelector(".edit-food-form").addEventListener("submit", editEntryHandler);
