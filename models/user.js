202ed1898ba271fd0e7faf87fe4ff43184a6d4ef
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-12-06 00:48:04 -0600

    debugging routes and login/signup.js

--- models/user.js
+++ models/user.js
@@ -17,13 +17,21 @@ User.init(
       allowNull: false,
       unique: true,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [8],
       },
-    },
+    }
   },
   {
     hooks: {
--- public/JS/login.js
+++ public/JS/login.js
@@ -1,45 +1,26 @@
-async function loginFormHandler(event) {
+const loginFormHandler = async (event) => {
   event.preventDefault();
-  // Collect values from the login form
-  const username = document.querySelector("#username-login").value.trim();
+
+  const email = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (username && password) {
-    const response = await fetch("/api/users/login", {
+  if (email && password) {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace("/");
     } else {
       alert("Failed to log in.");
     }
   }
-}
+};
 
-// const loginFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const username = document.querySelector("#username-login").value.trim();
-//   const password = document.querySelector("#password-login").value.trim();
-
-//   if (username && password) {
-//     const response = await fetch("/api/users/login", {
-//       method: "POST",
-//       body: JSON.stringify({ username, password }),
-//       headers: { "Content-Type": "application/json" },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace("/profile");
-//     } else {
-//       alert("Failed to log in.");
-//     }
-//   }
-// };
-
-document
-  .querySelector("#login-form")
-  .addEventListener("submit", loginFormHandler);
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#login-form")
+    .addEventListener("submit", loginFormHandler);
+});
